64a0ed3066657862f97d201bf6c5411698121e2b
cvc953 <cvcorporation05@gmail.com>
2025-05-26 16:21:10 -0500

    bug de requistro

--- lib/screens/registro.dart
+++ lib/screens/registro.dart
@@ -1,6 +1,8 @@
+import 'package:ap/screens/tareas.dart';
+import 'package:ap/screens/tareas_inicio.dart';
 import 'package:ap/widgets/login_input.dart';
 import 'package:flutter/material.dart';
-import 'package:ap/screens/paginaprincipal.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Registro extends StatelessWidget {
   Registro({super.key});
@@ -8,11 +10,62 @@ class Registro extends StatelessWidget {
   final email = TextEditingController();
   final password = TextEditingController();
   final password2 = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _register(BuildContext context) async {
+    if (password.text != password2.text) {
+      _showDialog(context, 'Las contraseñas no coinciden');
+      return;
+    }
+
+    try {
+      final UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(
+            email: email.text.trim(),
+            password: password.text.trim(),
+          );
+
+      if (userCredential.user != null) {
+        Navigator.of(
+          context,
+        ).pushReplacement(MaterialPageRoute(builder: (_) => TareasInicio()));
+      }
+    } on FirebaseAuthException catch (e) {
+      String message = 'Error desconocido';
+
+      if (e.code == 'email-already-in-use') {
+        message = 'El correo ya está en uso.';
+      } else if (e.code == 'invalid-email') {
+        message = 'Correo inválido.';
+      } else if (e.code == 'weak-password') {
+        message = 'La contraseña es muy débil.';
+      }
+
+      _showDialog(context, message);
+    }
+  }
+
+  void _showDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder:
+          (_) => AlertDialog(
+            title: Text('Registro fallido'),
+            content: Text(message),
+            actions: [
+              TextButton(
+                child: Text('Aceptar'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text("Registro")),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -24,21 +77,17 @@ class Registro extends StatelessWidget {
                 hintText: AutofillHints.email,
                 obscureText: false,
               ),
-
               LoginInput(
                 controller: password,
                 hintText: 'Contraseña',
                 obscureText: true,
               ),
-
               LoginInput(
                 controller: password2,
                 hintText: 'Repetir contraseña',
                 obscureText: true,
               ),
-
               SizedBox(height: 20),
-
               GestureDetector(
                 child: Container(
                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -47,40 +96,14 @@ class Registro extends StatelessWidget {
                     color: Colors.blueAccent,
                     borderRadius: BorderRadius.circular(10),
                   ),
-
                   child: Center(
                     child: Text(
-                      'Registrate',
+                      'Regístrate',
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
-                onTap: () {
-                  if (password.text != password2.text) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('Las contraseñas no coinciden'),
-                          actions: <Widget>[
-                            TextButton(
-                              child: Text('Aceptar'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  } else {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => Paginaprincipal(),
-                      ),
-                    );
-                  }
-                },
+                onTap: () => _register(context),
               ),
             ],
           ),
