071299f10682fb7b560f2bcf100f240213b13749
cvc953 <cvcorporation05@gmail.com>
2025-12-17 16:22:19 -0500

    fix: Update Gradle configuration and application namespace for consistency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
 }
 
 android {
-    namespace = "com.cvc.avas_eto"
+    namespace = "com.cvc953.avaseto"
     compileSdk = flutter.compileSdkVersion
     //ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cvc.avas_eto"
+        applicationId = "com.cvc953.avaseto"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         //minSdk = flutter.minSdkVersion
