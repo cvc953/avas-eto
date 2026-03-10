0d585d12ef9978b4483f456f015989536cc9589b
cvc953 <cvcorporation05@gmail.com>
2026-03-09 22:36:53 -0500

    feat: convert TareasList to StatefulWidget for improved state management and task handling

--- lib/screens/tareas_list.dart
+++ lib/screens/tareas_list.dart
@@ -2,7 +2,7 @@ import 'package:avas_eto/models/tarea.dart';
 import 'package:avas_eto/widgets/tarea_card.dart';
 import 'package:flutter/material.dart';
 
-class TareasList extends StatelessWidget {
+class TareasList extends StatefulWidget {
   final List<Tarea> tareas;
   final void Function(Tarea) onEliminar;
   final void Function(Tarea) onEditar;
@@ -17,8 +17,44 @@ class TareasList extends StatelessWidget {
   });
 
   @override
+  State<TareasList> createState() => _TareasListState();
+}
+
+class _TareasListState extends State<TareasList> {
+  late List<Tarea> _tareasLocales;
+
+  @override
+  void initState() {
+    super.initState();
+    _tareasLocales = List.from(widget.tareas);
+  }
+
+  @override
+  void didUpdateWidget(TareasList oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.tareas != oldWidget.tareas) {
+      _tareasLocales = List.from(widget.tareas);
+    }
+  }
+
+  void _handleCheck(Tarea tarea, bool completada) {
+    // Mover la tarea al final de la lista antes de llamar el callback
+    setState(() {
+      final index = _tareasLocales.indexWhere((t) => t.id == tarea.id);
+      if (index != -1) {
+        final tareaActualizada = _tareasLocales[index].copyWith(completada: completada);
+        _tareasLocales.removeAt(index);
+        _tareasLocales.add(tareaActualizada);
+      }
+    });
+
+    // Llamar el callback original
+    widget.onCheck(tarea, completada);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (tareas.isEmpty) {
+    if (_tareasLocales.isEmpty) {
       return Center(
         child: Text(
           'No hay tareas',
@@ -30,14 +66,14 @@ class TareasList extends StatelessWidget {
       slivers: [
         SliverList(
           delegate: SliverChildBuilderDelegate((context, index) {
-            final tarea = tareas[index];
+            final tarea = _tareasLocales[index];
             return TareaCard(
               key: ValueKey(tarea.id),
               tarea: tarea,
-              onCheck: (val) => onCheck(tarea, val ?? false),
-              onTap: () => onEditar(tarea),
+              onCheck: (val) => _handleCheck(tarea, val ?? false),
+              onTap: () => widget.onEditar(tarea),
             );
-          }, childCount: tareas.length),
+          }, childCount: _tareasLocales.length),
         ),
       ],
     );
